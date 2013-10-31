e7fde8c35598f4ea950ae2f7e10fd13cd5506b11
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-10-31 10:45:01 +0100

    HTML impl: Added JScript node to represent a script node in html

---
+++ src/main/scala/com.idyria.osi.vui.impl.html.js/JScript.scala
@@ -0,0 +1,9 @@
+package com.idyria.osi.vui.impl.html.js
+
+import com.idyria.osi.vui.core.styling.ApplyTrait
+
+class JScript(var content : String) extends ApplyTrait {
+  
+  type Self = JScript
+  
+}
